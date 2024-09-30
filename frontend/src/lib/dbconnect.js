9944386a46007d47b94b41ac722c57f1e5200203
Nnamdi Emmanuel Dike <emmanueldike275@gmail.com>
2024-09-30 23:50:01 +0100

    new push

--- frontend/src/lib/dbconnect.js
+++ frontend/src/lib/dbconnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/okay_mart";
+  process.env.MONGODB_URL || "mongodb://localhost:27017/okay_mart";
 
 if (!MONGODB_URI) {
   throw new Error(
