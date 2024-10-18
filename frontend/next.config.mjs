ebe30948ad46aacaeb2724fcc74c5d11f66ddcd1
Nnamdi Emmanuel Dike <emmanueldike275@gmail.com>
2024-10-18 13:02:07 +0100

    "Updated next.config.mjs to use remotePatterns for images and modified populate.js to increase number of products and change image generation"

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "avatars.githubusercontent.com"], // Add your allowed domains here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
   },
   webpack(config) {
     config.module.rules.push({
