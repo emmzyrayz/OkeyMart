690b06662fa984e34dc339c8b8b79230dbc08f0b
Nnamdi Emmanuel Dike <emmanueldike275@gmail.com>
2024-12-07 08:14:36 +0100

    `Update ESLint configuration and package.json for shop-service`

--- shop-service/.eslintrc.js
+++ shop-service/.eslintrc.js
@@ -1,26 +1,42 @@
+// .eslintrc.js
 module.exports = {
   env: {
     node: true,
     es2021: true,
-    commonjs: true,
   },
-  extends: ["eslint:recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "plugin:security/recommended",
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  plugins: ["node", "security"],
   rules: {
-    "no-unused-vars": "warn",
-    "no-console": "off",
-    "import/no-unresolved": "off",
-    "import/extensions": "off",
-    "import/no-case-sensitive": "off",
-  },
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    "node/exports-style": ["error", "module.exports"],
+    "node/file-extension-in-import": ["error", "always"],
+    "node/prefer-global/buffer": ["error", "always"],
+    "node/prefer-global/console": ["error", "always"],
+    "node/prefer-global/process": ["error", "always"],
+    "node/prefer-promises/dns": "error",
+    "node/prefer-promises/fs": "error",
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      {
+        version: ">=14.0.0",
+        ignores: [],
       },
-    },
+    ],
+    "security/detect-buffer-noassert": "error",
+    "security/detect-child-process": "error",
+    "security/detect-disable-mustache-escape": "error",
+    "security/detect-eval-with-expression": "error",
+    "security/detect-no-csrf-before-method-override": "error",
+    "security/detect-non-literal-fs-filename": "error",
+    "security/detect-non-literal-require": "error",
+    "security/detect-possible-timing-attacks": "error",
+    "security/detect-pseudoRandomBytes": "error",
   },
 };
